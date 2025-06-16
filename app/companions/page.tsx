3c0dd6906cc0de11ab91a292dcce3d8632e58b7f
Shubham Kamble <shubhamkamble8983@gmail.com>
2025-06-16 23:56:20 +0530

    Intial commit

---
+++ app/companions/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>Companion Library</div>;
+};
+
+export default page;
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Converso</h1>
+      <Button>Click me!</Button>
+    </div>
+  );
+};
+
+export default Page;
