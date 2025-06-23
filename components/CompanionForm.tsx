d01c5e089dd8494eef27cab9b0f26e4532e4718e
Shubham Kamble <shubhamkamble8983@gmail.com>
2025-06-23 23:53:16 +0530

    Implement Database setup, companion server actions

--- components/CompanionForm.tsx
+++ components/CompanionForm.tsx
@@ -22,8 +22,8 @@ import {
 } from "@/components/ui/select";
 import { subjects } from "@/constants";
 import { Textarea } from "@/components/ui/textarea";
-// import { createCompanion } from "@/lib/actions/companion.actions";
-// import { redirect } from "next/navigation";
+import { createCompanion } from "@/lib/actions/companion.actions";
+import { redirect } from "next/navigation";
 
 const formSchema = z.object({
   name: z.string().min(1, { message: "Companion is required." }),
@@ -48,13 +48,14 @@ const CompanionForm = () => {
   });
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    // const companion = await createCompanion(values);
-    // if (companion) {
-    //   redirect(`/companions/${companion.id}`);
-    // } else {
-    //   console.log("Failed to create a companion");
-    //   redirect("/");
-    // }
+    const companion = await createCompanion(values);
+
+    if (companion) {
+      redirect(`/companions/${companion.id}`);
+    } else {
+      console.log("Failed to create a companion");
+      redirect("/");
+    }
   };
 
   return (
