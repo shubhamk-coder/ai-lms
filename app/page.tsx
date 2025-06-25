fe9b98efab2893170ccd93696d9e3dfbfbbc5763
Shubham Kamble <shubhamkamble8983@gmail.com>
2025-06-26 00:06:59 +0530

    Implement search, sentry, subscription permissions, bookmarks, etc

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,46 @@
 import CompanionCard from "@/components/CompanionCard";
 import CompanionsList from "@/components/CompanionsList";
 import CTA from "@/components/CTA";
-import React from "react";
-import { recentSessions } from "@/constants";
+import {
+  getAllCompanions,
+  getRecentSessions,
+} from "@/lib/actions/companion.actions";
+import { getSubjectColor } from "@/lib/utils";
 
-const Page = () => {
+const Page = async () => {
+  const companions = await getAllCompanions({ limit: 3 });
+  const recentSessionsCompanions = await getRecentSessions(10);
+
   return (
-    <div>
-      <h1 className="text-2xl underline">Popular Companions</h1>
+    <main>
+      <h1>Popular Companions</h1>
+
       <section className="home-section">
-        <CompanionCard
-          id="1"
-          name="Neura: The Brainy Explorer"
-          topic="Neural Network of the Brain"
-          subject="science"
-          duration={45}
-          color="#ffda6e"
+        {companions.map((companion) => (
+          <CompanionCard
+            key={companion.id}
+            {...companion}
+            color={getSubjectColor(companion.subject)}
+          />
+        ))}
+      </section>
+
+      <section className="home-section">
+        <CompanionsList
+          title="Recently completed sessions"
+          companions={recentSessionsCompanions}
+          classNames="w-2/3 max-lg:w-full"
         />
-        <CompanionCard
-          id="2"
-          name="Countsy: The Number Wizard"
-          topic="Derivatives & Integrals"
-          subject="maths"
-          duration={30}
-          color="#e5d0ff"
-        />
-        <CompanionCard
-          id="3"
-          name="Verba: The Vocabulary Builder"
-          topic="English literature"
-          subject="language"
-          duration={30}
-          color="#bde7ff"
-        />
-        {/* <CompanionCard id="4" name="Captain Cosmos" topic="Exploring the Universe" subject="astronomy" duration={90} color="#ffd700"/>
+        <CTA />
+      </section>
+    </main>
+  );
+};
+
+export default Page;
+
+{
+  /* <CompanionCard id="4" name="Captain Cosmos" topic="Exploring the Universe" subject="astronomy" duration={90} color="#ffd700"/>
         <CompanionCard id="5" name="Dr. AI Expert" topic="Artificial Intelligence Fundamentals" subject="computer science" duration={75} color="#add8e6"/>
         <CompanionCard id="6" name="Dr. Data Detective" topic="Data Analysis Techniques" subject="statistics" duration={45} color="#f08080"/>
         <CompanionCard id="7" name="Dr. Code Conductor" topic="Programming Basics for Beginners" subject="programming" duration={60} color="#b0c4de"/>
@@ -41,19 +48,5 @@ const Page = () => {
         <CompanionCard id="9" name="Dr. Creative Catalyst" topic="Creative Writing and Storytelling" subject="literature" duration={45} color="#ffe4e1"/>
         <CompanionCard id="10" name="Dr. Fitness Frenzy" topic="Fitness Tips and Workouts" subject="health" duration={60} color="#90ee90"/>
         <CompanionCard id="11" name="Dr. Music Maestro" topic="Music Theory and Composition" subject="music" duration={45} color="#ffcccb"/>        
-        <CompanionCard id="12" name="Dr. Nature Explorer" topic="Exploring the Natural World" subject="biology" duration={90} color="#ffff00"/> */}
-      </section>
-
-      <section className="home-section">
-        <CompanionsList
-          title="Recently completed sessions"
-          companions={recentSessions}
-          classNames="w-2/3 max-lg:w-full"
-        />
-        <CTA />
-      </section>
-    </div>
-  );
-};
-
-export default Page;
+        <CompanionCard id="12" name="Dr. Nature Explorer" topic="Exploring the Natural World" subject="biology" duration={90} color="#ffff00"/> */
+}
--- components/CompanionCard.tsx
+++ components/CompanionCard.tsx
@@ -1,5 +1,9 @@
+"use client";
+import { removeBookmark } from "@/lib/actions/companion.actions";
+import { addBookmark } from "@/lib/actions/companion.actions";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 interface CompanionCardProps {
   id: string;
@@ -8,7 +12,9 @@ interface CompanionCardProps {
   subject: string;
   duration: number;
   color: string;
+  bookmarked: boolean;
 }
+
 const CompanionCard = ({
   id,
   name,
@@ -16,12 +22,21 @@ const CompanionCard = ({
   subject,
   duration,
   color,
+  bookmarked,
 }: CompanionCardProps) => {
+  const pathname = usePathname();
+  const handleBookmark = async () => {
+    if (bookmarked) {
+      await removeBookmark(id, pathname);
+    } else {
+      await addBookmark(id, pathname);
+    }
+  };
   return (
     <article className="companion-card" style={{ backgroundColor: color }}>
       <div className="flex justify-between items-center">
         <div className="subject-badge">{subject}</div>
-        {/* <button className="companion-bookmark" onClick={handleBookmark}>
+        <button className="companion-bookmark" onClick={handleBookmark}>
           <Image
             src={
               bookmarked ? "/icons/bookmark-filled.svg" : "/icons/bookmark.svg"
@@ -30,7 +45,7 @@ const CompanionCard = ({
             width={12.5}
             height={15}
           />
-        </button> */}
+        </button>
       </div>
 
       <h2 className="text-2xl font-bold">{name}</h2>
